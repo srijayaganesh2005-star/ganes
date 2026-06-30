ca816a916f7a4dff560608af66f9901e980cec44
srijayaganesh2005-star <srijayaganesh2005@gmail.com>
2026-06-30 11:54:08 +0530

    Add files via upload

---
+++ list/one.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class one {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                System.out.print(ch);
+            }
+        }
+    }
+}
